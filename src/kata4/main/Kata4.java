9829c1c037c44211042089266082f12cd0032b01
Entrar <Entrar@APD-L13>
2017-11-03 14:07:41 +0000

    Primera Versión de Kata4

---
+++ src/kata4/main/Kata4.java
@@ -0,0 +1,35 @@
+
+package kata4.main;
+import java.util.*;
+import kata4.model.*;
+import kata4.view.HistogramDisplay;
+import kata4.view.MailHistogramBuilder;
+import kata4.view.MailListReader;
+import java.io.IOException;
+
+public class Kata4 {
+    private static List<String> mailList;
+    private static Histogram<String> histogram;
+    
+    public static void main(String[] args) {
+        execute();
+    }
+
+    private static void execute(){
+        Input();
+        Process();
+        Output();
+    }
+
+    private static void Input(){
+        mailList = MailListReader.read("C:\\Users\\Entrar\\Documents\\NetBeansProjects\\Kata4\\src\\emailsfile.txt");
+    }
+
+    private static void Process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private static void Output(){
+        (new HistogramDisplay(histogram)).execute();
+    }
+}
